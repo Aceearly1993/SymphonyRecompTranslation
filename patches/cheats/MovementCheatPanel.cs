dc7b2f191ba6e45897f0b2bd50ed1e872c5ac18b
DerpPrincess <nekokagamines@gmail.com>
2026-08-08 20:56:46 -0500

    More Title Case Things

--- patches/cheats/MovementCheatPanel.cs
+++ patches/cheats/MovementCheatPanel.cs
@@ -59,17 +59,17 @@ public sealed class MovementCheatPanel : IPanel
         ImGui.SeparatorText("Speed"); //sp
         float speed = Player.Entity.VelocityX / (float)Cheats.One;
         ImGui.Text($"Velocity X: {speed:0.00} px/f   (default 1.00x)");
-        ImGui.Checkbox("Override speed", ref MovementCheat.SpeedOverride);
+        ImGui.Checkbox("Override Speed", ref MovementCheat.SpeedOverride);
         ImGui.SliderFloat("Multiplier", ref MovementCheat.SpeedMul, 0.1f, 5f, "%.2fx");
 
         ImGui.SeparatorText("Jump");
         float velY = Player.Entity.VelocityY / (float)Cheats.One;
         ImGui.Text($"Velocity Y: {velY:0.00} px/f");
         ImGui.SliderFloat("Strength", ref MovementCheat.JumpStrength, 1f, 16f, "%.1f px/f");
-        ImGui.Checkbox("Override jump", ref MovementCheat.JumpOverride);
+        ImGui.Checkbox("Override Jump", ref MovementCheat.JumpOverride);
 
         ImGui.SeparatorText("Toggles");
-        ImGui.Checkbox("Infinite jump (fly)", ref MovementCheat.InfiniteJump);
+        ImGui.Checkbox("Infinite Jump (fly)", ref MovementCheat.InfiniteJump);
         ImGui.Checkbox("No clip (press L2, then dpad)", ref MovementCheat.NoClip); //press or hold?
         ImGui.Checkbox("Invincible", ref MovementCheat.Invincible);
 
